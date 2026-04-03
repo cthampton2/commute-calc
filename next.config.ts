cf20d88bd1b692a13daade0948952bdede265367
cthampton2 <cthampton2@gmail.com>
2026-04-03 15:04:32 -0400

    again 4-3

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.context = path.resolve(__dirname);
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
