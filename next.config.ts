2e2c160bc46a2b01bd7444bea4ea7734716ebffe
cthampton2 <cthampton2@gmail.com>
2026-04-03 14:50:50 -0400

    Sitemap rebuild 4-3

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    resolveAlias: {
-      tailwindcss: path.resolve(__dirname, "node_modules/tailwindcss"),
-    },
-  },
   webpack(config) {
     config.context = path.resolve(__dirname);
     return config;
